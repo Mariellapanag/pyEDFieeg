7df2fed6c62209c8bae193ca9643ca2c4481e3f6
Mariella <mariella1990998@gmail.com>
2022-06-09 18:43:08 +0100

    ------------

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- Project information -----------------------------------------------------
 
@@ -67,7 +68,7 @@ html_context = {
     "github_user": "Mariellapanag",
     "github_repo": "pyEDFieeg",
     "github_version": "main",
-    #"conf_py_path": "/docs/source/",
+    "conf_py_path": "/docs/source/",
     "source_suffix": ".md",
 }
 
