5803339cd0504abbd07b23491852142e493f7149
Mariella <mariella1990998@gmail.com>
2022-11-28 14:44:36 +0000

    run and generate EDF info for remaining subjects

--- paths.py
+++ paths.py
@@ -29,7 +29,9 @@ PLOTS_BAD_CHANNELS_DIR = os.path.abspath(os.path.join(ALL_FILES_DIR, "PLOTS_BAD_
 # The location where edf files are
 #IN_EDF_DATA = os.path.abspath(os.path.join("/data1"))
 #D:\UCLH_edf
-IN_EDF_DATA = os.path.abspath(os.path.join("D:", "UCLH_edf"))
+# D:\UCLH\icEEG
+IN_EDF_DATA = os.path.abspath(os.path.join("D:", "UCLH\icEEG"))
+#IN_EDF_DATA = os.path.abspath(os.path.join("D:", "UCLH_edf"))
 #IN_EDF_DATA = os.path.abspath(os.path.join(ROOT_DIR, "..", "..", "yujiang", "Data", "UCLH", "icEEG"))
 #IN_EDF_DATA = os.path.abspath(os.path.join(ROOT_DIR, "..", "..", "yujiang", "Data", "GLAS", "icEEG"))
 
